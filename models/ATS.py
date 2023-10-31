282b46680a1ad3bcdc0d97425e119c411ccf7a05
danimap27 <danimp2002@gmail.com>
2023-10-31 17:00:55 +0100

    Solucion errores herencia

--- models/ATS.py
+++ models/ATS.py
@@ -2,11 +2,7 @@ from odoo import models, fields
 
 class ATS(models.Model):
     _name = 'veterinariaupo.ats'
-<<<<<<< HEAD
     _description = 'Modelo ATS'
-=======
-    _description = 'Modelo ATS (Técnico de Salud Animal)'
->>>>>>> 5b253f2 (Solucion error Herencia)
     _inherit = 'veterinariaupo.persona'
     
     idATS = fields.Char('ID ATS', size=9, required=True, help="Identificación del ATS")
