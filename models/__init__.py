ac28d49d37e65cdf589835022febaf670e7fac08
danimap27 <danimp2002@gmail.com>
2023-10-31 16:53:08 +0100

    Solucion error Herencia

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from . import Persona
 from . import ATS
 from . import Cita
 from . import Cliente
@@ -7,7 +8,6 @@ from . import Clinica
 from . import Laboratorio
 from . import Mascota
 from . import Medicina
-from . import Persona
 from . import PruebaMedica
 from . import Seguro
 from . import TipoMedicina
