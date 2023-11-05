8577f8641c25ed36236b30da14bc8ede1f69ee9f
Daniel Martin <74870961+danimap27@users.noreply.github.com>
2023-11-05 19:58:28 +0100

    Update reinicioBruto.sh

--- reinicioBruto.sh
+++ reinicioBruto.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-<<<<<<< HEAD
 
-docker compose stop
-
-#!/bin/bash
-
-=======
 cd ..
 cd ..
 docker compose stop
 
->>>>>>> 9dadd20 (atributos de la clase persona declarados)
 echo "Limpiando volúmenes Docker no utilizados..."
 docker volume prune -f
 
@@ -25,18 +18,12 @@ docker system prune -a
 
 echo "Reiniciando el servicio Docker..."
 sudo systemctl restart docker
-<<<<<<< HEAD
 
 
 echo "Operaciones completadas."
-
-=======
-if 
-
 sudo systemctl restart docker
 
 echo "Operaciones completadas."
 cd ..
 cd ..
->>>>>>> 9dadd20 (atributos de la clase persona declarados)
 docker compose up -d
