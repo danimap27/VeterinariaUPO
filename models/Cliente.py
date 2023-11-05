8c48e93a317be248802a438e8a62603200d9e730
Daniel Martin <74870961+danimap27@users.noreply.github.com>
2023-11-05 19:55:11 +0100

    Update Cliente.py

--- models/Cliente.py
+++ models/Cliente.py
@@ -3,8 +3,8 @@ from odoo import models, fields, api
 class Cliente(models.Model):
     _name = 'veterinariaupo.cliente'
     _description = 'Modelo Cliente de la clinica'
-    _inherit = ['veterinariaupo.persona']
+    _inherit = 'veterinariaupo.persona'
 
-    idCarnet = fields.Integer(int= "idCarnet", size = 9, required = True)
-    direccion = fields.String(String = "direccion", size = 20, required = True)
+    idCarnet = fields.Integer(int= 'idCarnet', size = 9, required = True)
+    direccion = fields.String(String = 'direccion', size = 20, required = True)
  
