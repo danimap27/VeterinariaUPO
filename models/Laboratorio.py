4b8b20d840a25e5dd68c90c9866f7f5e264b868e
VilchezPablo <146817969+VilchezPablo@users.noreply.github.com>
2023-11-07 16:50:27 +0100

    Update Laboratorio.py

--- models/Laboratorio.py
+++ models/Laboratorio.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api
 class Laboratorio(models.Model):
     _name = 'veterinariaupo.laboratorio'
     _description = 'Modelo del laboratorio'
-    #Atributos mascota
+    #Atributos laboratorio
     nombre = fields.Char(String = 'nombreLab', size = 256, required = True)
     direccion = fields.Char(String = 'direccionLaboratorio', required=True)
     correo = fields.Char(String = 'correo')
