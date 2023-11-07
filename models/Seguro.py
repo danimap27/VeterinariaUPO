aaffe0e009d04fdcd939981c66fd7182bb209fe7
VilchezPablo <146817969+VilchezPablo@users.noreply.github.com>
2023-11-07 17:04:48 +0100

    Update Seguro.py

--- models/Seguro.py
+++ models/Seguro.py
@@ -4,6 +4,7 @@ class Seguro(models.Model):
     _name = 'veterinariaupo.seguro'
     _description = 'Modelo del seguro'
     #Atributos Seguro
+    numeroPolica = fields.Integer(int="numPoliza", required = True, help = "Identificador Seguro") #Primary Key
     precio = fields.Float(float='precioSeguro', required = True)
     tipo = fields.Char(String = 'tipoSeguro', size = 256, required = True)
     condiciones = fields.Char(String = 'condicionesSeguro',autodate = True)
