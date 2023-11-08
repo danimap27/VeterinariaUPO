c47c5f872aa5eea9df41b7d92d74febb4c1200c9
Daniel Martin <74870961+danimap27@users.noreply.github.com>
2023-11-08 08:11:29 +0100

    vista menu creada

--- __manifest__.py
+++ __manifest__.py
@@ -23,9 +23,11 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        #'security/ir.model.access.csv',
+        'security/security.xml',
         'views/views.xml',
         'views/templates.xml',
+        'views/menu.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
