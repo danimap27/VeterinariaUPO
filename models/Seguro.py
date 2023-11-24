4b26467b2e14cbe75df26e040947b322ddacb900
VilchezPablo <146817969+VilchezPablo@users.noreply.github.com>
2023-11-24 16:29:58 +0100

    Correccion del campo autocalculado

--- models/Seguro.py
+++ models/Seguro.py
@@ -4,7 +4,7 @@ class Seguro(models.Model):
     _name = 'veterinariaupo.seguro'
     _description = 'Modelo del seguro'
     #Atributos Seguro
-    numeroPoliza = fields.Integer(int="numPoliza", required = True, help = "Identificador Seguro") #Primary Key
+    numeroPoliza = fields.Integer(int="numPoliza", required = True, help = "Identificador Seguro", compute='_calculoNumPoliza', store=True) #Primary Key
     precio = fields.Float(float='precioSeguro', required = True)
     tipo = fields.Char(String = 'tipoSeguro', size = 256, required = True)
     condiciones = fields.Char(String = 'condicionesSeguro',autodate = True)
@@ -12,7 +12,8 @@ class Seguro(models.Model):
     idMascota = fields.One2many('veterinariaupo.mascota','microChip','Mascota')
 
     _sql_constraints = [('numeroPoliza sqlConstr','UNIQUE (numeroPoliza)','Cada seguro tiene un numero de poliza distinto (primary key')]
-
+    
+    
     @api.depends('idMascota')
     def _calculoNumPoliza(self): 
             self.numeroPoliza = (self.idMascota * 2)
