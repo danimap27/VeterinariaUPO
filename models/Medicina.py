e2029ed41ca6979eed5c14fc45f0e4bdc8f5c77e
davigalo <135728618+davigalo@users.noreply.github.com>
2023-12-01 15:35:54 +0100

    Validaciones y clave primaria Medicina.py

--- models/Medicina.py
+++ models/Medicina.py
@@ -1,19 +1,42 @@
-from odoo import models, fields
+from odoo import models, fields,api
+from datetime import datetime
 
+import re
 class Medicina(models.Model):
     _name = 'veterinariaupo.medicina'
     _description = 'Modelo Medicina'
-    _inherit = 'veterinariaupo.persona'
     
-    codigo_nacional = fields.Integer('Id Medicina',required=True, help = "Identificador de medicina" ) #Primary key
+    codigo_nacional = fields.Float('Id Medicina',digits=(6,1),required=True, help = "Identificador de medicina" ) #Primary key
     nombre = fields.Char('Nombre medicina', size=20, required=True, help="nombre de medicina")
     precio = fields.Float('Precio',(3,2))
-    caducidad = fields.Datetime('Caducidad',required=True, autodate = True )
+    caducidad = fields.Date('Caducidad',required=True, autodate = True )
     descripcion = fields.Text('Descripcion')
     prospecto = fields.Char('Prospecto',size = 20,required=True)
-  #relaciones
+   #relaciones
     laboratorios_id = fields.Many2many('veterinariaupo.laboratorio',string='codigo_nacional')
     tratamiento_id = fields.Many2one('veterinariaupo.tratamiento', string='Tratamiento')
     tipos_medicina_id = fields.One2many('veterinariaupo.tipomedicina','medicinas_ids', string='Tipo de Medicinas')
     
+    _sql_constraints = [('codigo_nacional_sqlConstr','UNIQUE (codigo_nacional)','Cada medicina tiene un codigo distinto (primary key')]
+
+   #validaciones
    
+   
+    
+    @api.constrains('caducidad')
+    def checkfecha(self):
+      if not self.validaCaducidad(self.caducidad):
+        raise models.ValidationError('LA fecha esta en el pasado')
+        
+    @api.onchange('precio')
+    def checkprecio(self):
+      if self.precio < 0:
+        raise models.ValidationError('El precio no puede ser menor que 0')
+    
+
+   
+    def validaCaducidad(self,caducidad):
+      fecha_actual = fields.Date.context_today(self)
+      return caducidad > fecha_actual
+     
+      
