377d0d6477e99a1fd4ed59bf0a10d95a5bf9eca7
Juan Rivas <72010616+rivasibanezjuan@users.noreply.github.com>
2023-12-03 14:51:11 +0100

    Update PruebaMedica.py

--- models/PruebaMedica.py
+++ models/PruebaMedica.py
@@ -1,17 +1,31 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class PruebaMedica(models.Model):
     _name = 'veterinariaupo.pruebamedica'
     _description = 'Modelo PruebaMedica'
 
-    
-    id_pruebamedica = fields.Integer('numPruebaMedica',required=True, help = "Identificador de prueba medica" ) #Primary key
-    tipo = fields.Char(String = 'tipoPruebaMedica', size = 256, required = True)
-    fecha = fields.Datetime('Fecha',required=True, autodate = True ) 
-    descripcion = fields.Text('Descripcion')
+    id_pruebamedica = fields.Integer('Número de Prueba Médica', required=True, help="Identificador de prueba medica")  # Primary key
+    tipo = fields.Char(string='Tipo de Prueba Médica', size=256, required=True)
+    fecha = fields.Datetime('Fecha', required=True, autodate=True)
+    descripcion = fields.Text('Descripción')
 
-    # Relación con la clase ATS (Muchos a 1)
     ats_id = fields.Many2one('veterinariaupo.ats', string='ATS')
-
-    # Relación con la clase Cita (Muchos a 1)
     cita_id = fields.Many2one('veterinariaupo.cita', string='Cita')
+
+    @api.constrains('fecha')
+    def check_fecha(self):
+        # Validar que la fecha de la prueba médica no esté en el pasado
+        if self.fecha < fields.Datetime.now():
+            raise models.ValidationError('La fecha de la prueba médica no puede estar en el pasado.')
+
+    @api.constrains('tipo')
+    def check_tipo(self):
+        # Validar que el tipo de prueba médica no contenga caracteres especiales
+        if not self.tipo.isalnum():
+            raise models.ValidationError('El tipo de prueba médica no puede contener caracteres especiales.')
+
+    @api.constrains('descripcion')
+    def check_descripcion(self):
+        # Validar que la descripción no sea demasiado corta
+        if len(self.descripcion) < 5:
+            raise models.ValidationError('La descripción de la prueba médica debe tener al menos 5 caracteres.')
