b3f16930637033199651c0c8e483fe55adc8965d
Daniel Martin <74870961+danimap27@users.noreply.github.com>
2023-12-05 12:08:44 +0100

    onchange y constrain de ats
    Tambien en la parte demos he intentado solucionar un error de la id de una mascota

--- models/ATS.py
+++ models/ATS.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ATS(models.Model):
     _name = 'veterinariaupo.ats'
@@ -10,3 +10,28 @@ class ATS(models.Model):
     
     prueba_medicas = fields.One2many('veterinariaupo.pruebamedica', 'ats_id', string='Pruebas médicas')
     laboratorios_id = fields.Many2one('veterinariaupo.laboratorio', string='Laboratorio')
+
+    _sql_constraints = [
+        ('unique_idATS', 'unique(idATS)', 'El ID ATS debe ser único.'),
+    ]
+    
+    @api.onchange('idATS')
+    def _onchange_idATS(self):
+        if self.idATS:
+            self.name = f"ATS - {self.idATS}"
+
+    @api.onchange('especialidad')
+    def _onchange_especialidad(self):
+        if self.especialidad:
+            self.especialidad = self.especialidad.upper()
+
+    @api.onchange('prueba_medicas')
+    def _onchange_prueba_medicas(self):
+        if self.prueba_medicas:
+            total_cost = sum(prueba.costo for prueba in self.prueba_medicas)
+            self.total_costo_pruebas = total_cost
+
+    @api.onchange('laboratorios_id')
+    def _onchange_laboratorios_id(self):
+        if self.laboratorios_id:
+            self.direccion_laboratorio = self.laboratorios_id.direccion
